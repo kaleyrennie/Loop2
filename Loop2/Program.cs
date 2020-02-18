bf0500688e761a41c31f8afc2998258285665a05
Kaley <kaleyrennie14@gmail.com>
2020-02-17 16:19:51 -0800

    Add project files.

---
+++ Loop2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many pets do I have?");
+            int pet = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+
+
+                switch (pet)
+                {
+
+                    case 1:
+                        Console.WriteLine("Correct! I have 1 pet.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.Write("You are wrong. ");
+                        Console.Write("How many pets do I have?");
+                        pet = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
